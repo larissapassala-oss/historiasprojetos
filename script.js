b60aa89f889277af01e9b82bc6b8b27056d97d84
larissapassala-oss <larissa.passala@escola.pr.gov.br>
2025-10-21 08:34:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca= document.querySelectorAll("btn-proximo")
+
+avanca-array.forEach(buton=> {
+    Button.addEventlistener("click", function(){
+        const-atual = document.querySelector(" ativo");
+        const proXimopasso
+
+    }
+    
+});
